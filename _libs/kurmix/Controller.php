6422b1b0edc5c1047b1c23c441129f182b722540
Andree Ochoa <andlody@hotmail.com>
2018-07-19 11:29:06 -0500

    prueba

--- _libs/kurmix/Controller.php
+++ _libs/kurmix/Controller.php
@@ -5,7 +5,7 @@
 * @license   The MIT license                        | . \ |_| | |  | | | | | | |>  < 
 * @version   1.0.0                                  |_|\_\__,_|_|  |_| |_| |_|_/_/\_\       */
 
-class Controller {
+class Controller {hh
 		
 	protected $vista;
 	protected $band;
--- app/model/Usuarios.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Usuarios extends Model {
-	//Si necesitas ayuda, ingresa a la web: https://www.kurmix.org/?k=manual/base_de_datos 		
-}
